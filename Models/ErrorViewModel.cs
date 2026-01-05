e5f44d53be4783a009af09a25779ef3c4806241c
abdirazak11 <zakzz7709@gmail.com>
2026-01-05 16:31:56 +0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolNotificationSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
