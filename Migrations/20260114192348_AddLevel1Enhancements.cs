0ea29560113f1f8e2bbc487db8764104681c8cae
abdirazak11 <zakzz7709@gmail.com>
2026-01-14 23:35:36 +0400

    creating new cshtml files and updated database also

---
+++ Migrations/20260114192348_AddLevel1Enhancements.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolNotificationSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLevel1Enhancements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
